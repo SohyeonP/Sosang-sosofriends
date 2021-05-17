c071155b33a5ba4510d6a178741e4e277a32a12e
SohyeonP <hidhen9714@naver.com>
2021-05-17 16:58:22 +0900

    add postsql 설치

--- src/main/resources/static/assets/js/common.js
+++ src/main/resources/static/assets/js/common.js
@@ -5,32 +5,27 @@ common={
         var itemname = $("#name").val();
         var kakaoitems = $("form[name=itemform]").serializeToString();
         console.log(kakaoitems);
-        /*$.ajax({
+        
+        $.ajax({
             type: "POST",
             url: "../additems.do",
             dataType: "json",
             data: kakaoitems,
             success: function(data){
-                if(data.isSuccessful){
-                    $("#api_key").text(data.body.apikey);
-                    showalarm('reissuance');
-                } else {
-                    if(data.status.code == "E10") {
-                        alert("카카오 아이템"+itemname+"이 등록 되었습니다");
-                        window.location.reload();
-                    } else {
-                        alert("카카오 아이템"+itemname+"이 등록실패하였습니다.");
-                        window.location.reload();
-                    }
-                }
-
-            },
+	
+				if (data.status.code == "200") {
+					alert("카카오 아이템" + itemname + "이 등록 되었습니다");
+					window.location.reload();
+				} else {
+						alert("카카오 아이템" + itemname + "이 등록실패 되었습니다");
+					window.location.reload();
+				}
+			},
             error: function(err){
-                alert("카카오 아이템"+itemname+"이 등록실패하였습니다. 에러남!");
+                alert("Error:카카오 아이템"+itemname+"이 등록실패!!");
                 console.log(err);
 
             }
-        });*/
-
+        });
     }
 }
