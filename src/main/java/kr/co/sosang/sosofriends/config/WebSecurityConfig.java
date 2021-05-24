488b92fc747e7a4d57f89043335613b5fd951d01
SohyeonP <hidhen9714@naver.com>
2021-05-24 09:06:33 +0900

    commit 20210524

--- src/main/java/kr/co/sosang/sosofriends/config/WebSecurityConfig.java
+++ src/main/java/kr/co/sosang/sosofriends/config/WebSecurityConfig.java
@@ -16,27 +16,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-<<<<<<< Updated upstream
+
+        http.cors().and().csrf().disable();
         http    .authorizeRequests()
-                .antMatchers("/login").authenticated()
+                .antMatchers("/login").authenticated().antMatchers("/**").permitAll();
                 .antMatchers("/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/css/**", "/js/**","img/main/**").permitAll()
                 .and().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .and().csrf().disable();
-        http    .formLogin().disable()
+                .and()).disable().anonymous().authorities("ROLE_ANONYMOUS").and().authorizeRequests();
+        http   .httpBasic().disable()	
                 .anonymous().authorities("ROLE_ANONYMOUS")
                 .and().httpBasic().disable();
-=======
-		http.cors().and().csrf().disable();
-		http.authorizeRequests();
-		http.formLogin() .permitAll();
-		http.authorizeRequests().antMatchers("/login").authenticated().antMatchers("/**").permitAll();
-		http.csrf().disable().anonymous().authorities("ROLE_ANONYMOUS").and().authorizeRequests()
-				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
-
-		http.httpBasic().disable();
-        
->>>>>>> Stashed changes
     }
 
 
